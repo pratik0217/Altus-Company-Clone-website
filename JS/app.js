648c89469c1e765368d163387f14ff78f7cee047
pratik0217 <jadhavpratik0208@gmail.com>
2025-06-04 00:10:07 +0530

    This project is finish.

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,13 @@
-// const welcomeMsg = setTimeout(function() {
-//     alert(`Hello, Welcome to Altus Company world.`);
-// },2000);
+const welcomeMsg = setTimeout(function() {
+    alert(`Hello, Welcome to Altus Company world`);
+    alert("this is clone of Altus Solution Company")
+},2000);
 
 
-function showMessage() {
-    alert("Not accessible this link");
+function contactMessage() {
+    confirm("The available developer contact information");
+
+    if(true) {
+        alert("Email Id - jadhavpratik0208@gmail.com");
+    }
 }
